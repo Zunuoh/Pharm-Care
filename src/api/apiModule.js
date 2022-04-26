dbe001daaf67998160a21a658531c7d37430c5d7
Zunuoh <audponu@gmail.com>
2022-04-26 23:07:30 +0000

    added https to server url to prevent CORS error

--- src/api/apiModule.js
+++ src/api/apiModule.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const getDrugs = async () => {
-    const response = await axios("http://www.mocky.io/v2/5c3e15e63500006e003e9795")
-
-     // console.log(response)
-    return response['data'];
-}
+  const response = await axios(
+    "https://www.mocky.io/v2/5c3e15e63500006e003e9795"
+  );
+  return response["data"];
+};
 
 export const ApiModule = {
-    getDrugs
+  getDrugs,
 };
